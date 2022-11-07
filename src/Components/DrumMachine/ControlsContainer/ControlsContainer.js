e227caf6a40e44119962e7d42026cef77b3ba004
Mike Oram <Mikeoram01@gmail.com>
2022-11-07 11:53:23 +0000

    Created and styled containers for drum pad and controls

---
+++ src/Components/DrumMachine/ControlsContainer/ControlsContainer.js
@@ -0,0 +1,10 @@
+import './ControlsContainer.css'
+
+const ControlsContainer = () => {
+    return (
+        <div className={"controls-container"}>
+        </div>
+    )
+}
+
+export default ControlsContainer
--- src/Components/DrumMachine/DrumMachine.js
+++ src/Components/DrumMachine/DrumMachine.js
@@ -1,10 +1,17 @@
 import './DrumMachine.css'
 import DrumPadContainer from "./DrumPadContainer/DrumPadContainer";
+import ControlsContainer from "./ControlsContainer/ControlsContainer";
+import {useState} from "react";
 
 const DrumMachine = () => {
+
+    const [displayText, setDisplayText] = useState('Snare')
+    const [power, setPower] = useState(true)
+
     return (
         <div id={"drum-machine"} className={"drum-machine"}>
             <DrumPadContainer />
+            <ControlsContainer displayText={displayText} setPower={setPower}/>
         </div>
     )
 }
